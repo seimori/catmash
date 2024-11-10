4c881070e2b0ec3ff14e8615efffde3de3da68d0
seimori <issei.morimoto@gmail.com>
2024-11-11 00:28:19 +0100

    ➕ install and setup react query

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Footer } from "../components/layout/Footer/Footer";
 import { Header } from "../components/layout/Header/Header";
 import { MainContent } from "../components/layout/MainContent/MainContent";
+import Providers from "../components/utils/Providers";
 import "./globals.css";
 
 export default function RootLayout({
@@ -11,9 +12,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Header />
-        <MainContent>{children}</MainContent>
-        <Footer>Voir le classement des chats</Footer>
+        <Providers>
+          <Header />
+          <MainContent>{children}</MainContent>
+          <Footer>Voir le classement des chats</Footer>
+        </Providers>
       </body>
     </html>
   );
