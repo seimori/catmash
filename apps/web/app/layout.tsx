ab2be1dc531c4a061e0a8fa67cae1e4862eabaf3
seimori <issei.morimoto@gmail.com>
2024-11-12 02:38:33 +0100

    🐛 fix error relating to font configuration

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,11 +1,5 @@
 import { Header } from "../components/layout/Header/Header";
-import Providers from "../components/utils/Providers";
 import "./globals.css";
-import { Inter } from "next/font/google";
-
-const inter = Inter({
-  subsets: ["latin"],
-});
 
 export default function RootLayout({
   children,
@@ -13,7 +7,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={inter.className}>
+    <html lang="en">
       <body>
         <Header />
         {children}
---
+++ apps/web/pages/_app.tsx
@@ -0,0 +1,14 @@
+import { AppProps } from "next/app";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+});
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <main className={inter.className}>
+      <Component {...pageProps} />
+    </main>
+  );
+}
