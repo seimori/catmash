8de0b075ae614d64a8c7bd9d4da302b7863a8fdb
seimori <issei.morimoto@gmail.com>
2024-11-12 11:47:41 +0100

    ⚡️ revalidate rankings every 60 seconds

--- apps/web/utils/actions/getRankedCats.ts
+++ apps/web/utils/actions/getRankedCats.ts
@@ -2,7 +2,8 @@ import { Cat } from "../../types";
 
 export async function getRankedCats(): Promise<Cat[]> {
   const response = await fetch(
-    `${process.env.API_URL}/cats?limit=100&orderByScore=desc`
+    `${process.env.API_URL}/cats?limit=100&orderByScore=desc`,
+    { next: { revalidate: 60 } }
   );
   if (!response.ok) {
     throw new Error("Error while fetching cats ranking");
