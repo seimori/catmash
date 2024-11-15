e94a839f97c3379120ef44f511afb4d4a97845be
seimori <issei.morimoto@gmail.com>
2024-11-15 12:33:44 +0100

    🐛 remove unused import

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,7 +1,6 @@
 import { Header } from "../components/layout/Header/Header";
 import "./globals.css";
 import { inter } from "../public/fonts/inter";
-import { roboto } from "../public/fonts/roboto";
 
 export default function RootLayout({
   children,
