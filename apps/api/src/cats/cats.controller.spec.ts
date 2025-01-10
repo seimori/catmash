0ba300f6c9afff640a11f383259752cf7477fb4a
seimori <issei.morimoto@gmail.com>
2025-01-11 00:30:47 +0100

    ✅ add tests for cats.controller

--- apps/api/src/cats/cats.controller.spec.ts
+++ apps/api/src/cats/cats.controller.spec.ts
@@ -11,7 +11,11 @@ describe('CatsController', () => {
   ];
 
   const mockCatsService = {
-    findAll: (limit: number, orderByScore: string) => mockCatsList,
+    findAll: jest.fn((limit: number, orderByScore: string) => mockCatsList),
+
+    findAllRandom: jest.fn((limit: number) => mockCatsList),
+
+    findOne: jest.fn((id: number) => mockCatsList[0]),
   };
 
   beforeEach(async () => {
@@ -27,9 +31,49 @@ describe('CatsController', () => {
     expect(controller).toBeDefined();
   });
 
-  it('should findAll', () => {
-    expect(controller.findAll({ limit: 1, orderByScore: 'ASC' })).toBe(
-      mockCatsList,
-    );
+  describe('GET /', () => {
+    it('should findAll when no options passed', () => {
+      expect(controller.findAll({})).toBe(mockCatsList);
+      expect(mockCatsService.findAll).toHaveBeenCalledWith(
+        undefined,
+        undefined,
+      );
+    });
+
+    it('should findAll when all options passed', () => {
+      expect(controller.findAll({ limit: 1, orderByScore: 'ASC' })).toBe(
+        mockCatsList,
+      );
+      expect(mockCatsService.findAll).toHaveBeenCalledWith(1, 'ASC');
+    });
+
+    it('should findAll when limit passed', () => {
+      expect(controller.findAll({ limit: 1 })).toBe(mockCatsList);
+      expect(mockCatsService.findAll).toHaveBeenCalledWith(1, undefined);
+    });
+
+    it('should findAll when orderByScore passed', () => {
+      expect(controller.findAll({ orderByScore: 'DESC' })).toBe(mockCatsList);
+      expect(mockCatsService.findAll).toHaveBeenCalledWith(undefined, 'DESC');
+    });
+  });
+
+  describe('GET /random', () => {
+    it('should findAllRandom when no options passed', () => {
+      expect(controller.findAllRandom({})).toBe(mockCatsList);
+      expect(mockCatsService.findAllRandom).toHaveBeenCalledWith(undefined);
+    });
+
+    it('should findAllRandom when limit passed', () => {
+      expect(controller.findAllRandom({ limit: 1 })).toBe(mockCatsList);
+      expect(mockCatsService.findAllRandom).toHaveBeenCalledWith(1);
+    });
+  });
+
+  describe('GET /:id', () => {
+    it('should findOne', () => {
+      expect(controller.findOne('1')).toBe(mockCatsList[0]);
+      expect(mockCatsService.findOne).toHaveBeenCalledWith(1);
+    });
   });
 });
